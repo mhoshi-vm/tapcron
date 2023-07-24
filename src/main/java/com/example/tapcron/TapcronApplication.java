d6e637afb2f32c2476e92342a733036dcf437d5c
machih <machih@vmware.com>
2023-07-24 15:36:00 +0900

    Update

--- src/main/java/com/example/tapcron/TapcronApplication.java
+++ src/main/java/com/example/tapcron/TapcronApplication.java
@@ -2,12 +2,19 @@ package com.example.tapcron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class TapcronApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TapcronApplication.class, args);
+    }
+
+    @PostMapping ("/")
+    public void post(){
+        System.out.println("New Event Came!");
     }
 
 }
