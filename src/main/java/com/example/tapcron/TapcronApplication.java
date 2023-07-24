01804ca783257bb519cbc77804ba796723ff6026
machih <machih@vmware.com>
2023-07-24 15:35:02 +0900

    Initial commit

---
+++ src/main/java/com/example/tapcron/TapcronApplication.java
@@ -0,0 +1,13 @@
+package com.example.tapcron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapcronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapcronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tapcron/TapcronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tapcron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapcronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
